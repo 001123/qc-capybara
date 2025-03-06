239954542c454d8c466e379f72642ea56d896e90
001123 <khuongduy1897@gmail.com>
2025-03-06 07:42:15 +0700

    feat:  add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Capybaraaaa",
@@ -13,10 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="vi">
       <body>{children}</body>
 
       <Toaster position="top-center" reverseOrder={false} />
+      <Analytics />
     </html>
   );
 }
